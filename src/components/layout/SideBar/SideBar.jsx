4a583c40574fe67ef7d154fe0d99d55ca73ffb64
Marin322 <santik.987@mail.ru>
2026-02-06 11:06:29 +0500

    Сделан SideBar и вынес LevelProgressBar в отдельный компонент

---
+++ src/components/layout/SideBar/SideBar.jsx
@@ -0,0 +1,5 @@
+export function SideBar() {
+    return (
+        <div className="w-full h-full bg-background-secondary border-r-2 border-border-secondary"></div>
+    );
+};
---
+++ src/components/ui/index.js
@@ -0,0 +1,1 @@
+export * from './userUi/LevelProgressBar';
